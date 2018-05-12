77b421cae5f9ceab4eae47f44ffed33ab74fca8f
Felipe Braiani <felipe.braianitech@gmail.com>
2018-05-12 01:17:14 -0400

    Initial version with Voyager done

---
+++ app/Edicao.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Edicao extends Model
+{
+    protected $table = 'edicoes';
+    public $timestamps = false;
+}
---
+++ app/Widgets/CandidatosDimmer.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Widgets;
+
+use Arrilot\Widgets\AbstractWidget;
+use Illuminate\Support\Str;
+use App\Candidato;
+
+class CandidatosDimmer extends AbstractWidget
+{
+    /**
+     * The configuration array.
+     *
+     * @var array
+     */
+    protected $config = [];
+
+    /**
+     * Treat this method as a controller action.
+     * Return view() or other content to display.
+     */
+    public function run()
+    {
+        $count = Candidato::count();
+
+        return view('voyager::dimmer', array_merge($this->config, [
+            'icon'   => 'voyager-group',
+            'title'  => "{$count} Candidatos",
+            'text'   => "{$count} candidatos realizaram as provas de 2009 a 2016 e estão cadastrados no sistema.",
+            'button' => [
+                'text' => 'Ver todos os candidatos',
+                'link' => route('voyager.candidatos.index'),
+            ],
+            'image' => 'candidatosdimmer.jpg',
+        ]));
+    }
+}
