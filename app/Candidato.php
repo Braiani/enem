826159dcbae341185c1e0248523778251a6e27ed
Felipe Braiani <felipe.braianitech@gmail.com>
2018-05-13 23:41:41 -0400

    Added the notSearchable list to the Candidatos browse view.

--- app/Candidato.php
+++ app/Candidato.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Candidato extends Model
 {
     public $timestamps = false;
+
+    public function getNotSearchableAttribute()
+    {
+        return ['id', 'edicao_id', 'nu_inscricao', 'cn', 'ch', 'lc', 'mat', 'red'];
+    }
 }
